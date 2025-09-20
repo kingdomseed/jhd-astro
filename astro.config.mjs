a73edf671f92a280e22f0b912abc273772fcb2d3
Jason Holt <jason@jasonholtdigital.com>
2025-09-20 09:47:31 +0200

    chore: migrate from Azure Static Web Apps to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,22 @@ import { defineConfig } from "astro/config";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // Astro configuration for this demo project.  The default settings are
 // sufficient for static sites, but a config file is still provided so
 // that a user dropping these files into their own project has a
 // reference point.  See https://docs.astro.build/en/reference/configuration/
 // for more details.
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()]
+  integrations: [sentry(), spotlightjs()],
+  adapter: cloudflare(),
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+      config: {
+        mode: "compile"
+      }
+    }
+  },
 });
